307429909a1b45739283e89ae215991009069ff3
Leela05 <leelarenu05@gmail.com>
2022-03-31 12:26:40 +0530

    testing form

---
+++ test_form.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import pytest
+
+@pytest.fixture
+def setUp():
+    global driver,name
+    name = input("Enter Name:")
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    yield
+    time.sleep(5)
+    driver.close()
+
+def test_form(setUp):
+    driver.get("https://iprimedtraining.herokuapp.com/")
+    time.sleep(2)
+    driver.find_element_by_name("name").send_keys(name)
+    time.sleep(2)
+    driver.find_element_by_xpath("/html/body/div/div/div[2]/form/table/tbody/tr[2]/td[2]/input[2]").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("/html/body/div/div/div[2]/form/table/tbody/tr[3]/td[2]/select/option[1]").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("/html/body/div/div/div[2]/form/table/tbody/tr[4]/td[2]/div/input").click()
+    time.sleep(2)
+    driver.find_element_by_name("submit").click()
+    time.sleep(2)
+
